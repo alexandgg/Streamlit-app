a8df846117cd06f34b449867571e939c1fc50d82
IcyDingo <78955247+IcyDingo@users.noreply.github.com>
2021-04-01 19:19:08 +0400

    deleted sklean

--- dashboard.py
+++ dashboard.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px 
-from sklearn.decomposition import PCA
+
 
 #Set Full Page Width
 st.set_page_config(layout="wide")
